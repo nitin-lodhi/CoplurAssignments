a4eabad36be05b509e8bd3ec77a5adcdfc5ac307
Nitin Lodhi <nitinlodhi101@gmail.com>
2025-03-05 12:33:30 +0530

    first commit

---
+++ Assignment2/task1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+// Using interface 
+interface ILogger{
+      public void Log();
+}
+
+class DatabaseLogger: ILogger{
+      public void Log(){
+        Console.WriteLine("Log Written to Database.");
+      }
+}
+
+class FileLogger: ILogger{
+      public void Log(){
+        Console.WriteLine("Log Written to File.");
+      }
+}
+
+class CloudLogger: ILogger{
+      public void Log(){
+        Console.WriteLine("Log Written to Cloud.");
+      }
+}
+
+
+class Program{
+
+    static ILogger GetLogger(string userLog){
+        if(userLog == "Database"){
+             return new DatabaseLogger();
+        }else if(userLog == "File"){
+             return new FileLogger();
+        }else if(userLog == "Cloud"){
+              return  new CloudLogger();
+        }
+        return null;
+    }
+
+    static void Main(){
+
+        ILogger log;
+        
+        Console.WriteLine("Enter the Log type(Database,File,Cloud): ");
+        string userLog = Console.ReadLine();
+
+        log =  GetLogger(userLog);
+
+        if(log != null){
+            log.Log();
+        }else{
+            Console.WriteLine("Invalid Log");
+        }
+        
+    }
+}
+
---
+++ Assignment2/task2/Program.cs
@@ -0,0 +1,39 @@
+﻿// using System;
+// 1. Using Abstract (Runtime Polymorphism)
+abstract class Logger{
+      public abstract void Log();
+}
+
+class Database: Logger{
+      public override void Log(){
+        Console.WriteLine("Log Written to Database.");
+      }
+}
+
+class File: Logger{
+      public override void Log(){
+        Console.WriteLine("Log Written to File.");
+      }
+}
+
+class Cloud: Logger{
+      public override void Log(){
+        Console.WriteLine("Log Written to Cloud.");
+      }
+}
+
+
+class Program{
+
+      
+    static void Main(){
+
+        Logger log1 = new Database();
+        Logger log2 = new File();
+        Logger log3 = new Cloud();
+
+        log1.Log();
+        log2.Log();
+        log3.Log(); 
+    }
+}
